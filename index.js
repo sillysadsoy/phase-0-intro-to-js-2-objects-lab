101945c455e8e155c9c51e20d293acf2aa1f412f
roger chat <mattrogerpelosi@gmail.com>
2021-07-10 16:09:16 -0400

    Assignment Complete

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const employee = {
     name: '',
     streetAddress: ''
-}
+};
 
 function updateEmployeeWithKeyAndValue(employee, key, value) {
     return Object.assign({}, employee, {[key]: value});
-}
+};
 
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
     return Object.assign(employee, {[key]: value});
-}
+};
 
 function deleteFromEmployeeByKey(employee, key) {
-    Object.assign = ({}, employee[key]);
-    return {}; 
-}
+    const newNew = {};
+    Object.assign (newNew, employee);
+    delete newNew[key];
+    return newNew; 
+};
 
 function destructivelyDeleteFromEmployeeByKey(employee, key) {
     delete employee[key];
     return employee;
-}
+};
